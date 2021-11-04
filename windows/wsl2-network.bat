9dc7bbebd6b97bd5c675052e9007f3dbd876c1d6
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-11-04 16:25:35 +0000

    Update wsl2-network script

--- windows/wsl2-network.bat
+++ windows/wsl2-network.bat
@@ -1,6 +1,7 @@
-@REM Fix network (port forwarding)
+@REM Fix network
 powershell -executionpolicy bypass -file C:\tools\wsl2-network.ps1
 
-@REM Launch WSL services
+@REM Start WSL and launch services
+wsl --shutdown
 wsl -u root -- service ssh start
 wsl -u root -- service docker start
